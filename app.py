1a86e4de8732a42d8273da6b797dee72095b5821
PATH456 <thanhhaipham652@gmail.com>
2024-07-02 16:37:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+Exercise 2: Print the sum of the current number and the previous number
+Write a program to iterate the first 10 numbers, and in each iteration, print the sum of the current and previous number.
+
+    
 print('Printing current and previous number sum in a range(10)')
 previous_number = 0
 for i in range(10):
