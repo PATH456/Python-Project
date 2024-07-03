4b0fc4348c9f726f384a5d4a259972cb9b8506b7
PATH456 <thanhhaipham652@gmail.com>
2024-07-03 10:18:47 +0700

    Update exercise7.py

--- exercise7.py
+++ exercise7.py
@@ -1,3 +1,6 @@
+Exercise 7: Return the count of a given substring from a string
+Write a program to find how many times substring “Emma” appears in the given string.
+  
 string = 'Henry is a good guy. Henry is hard-working and has another friend whose name is Henry'
 cnt = string.count('Henry')
 print(f"Henry appears {cnt} times")
