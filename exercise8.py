c8a9f72ab07ba3898680eeeb29f4f56f5d0f9a66
PATH456 <thanhhaipham652@gmail.com>
2024-07-03 11:02:25 +0700

    Update exercise8.py

--- exercise8.py
+++ exercise8.py
@@ -1,4 +1,13 @@
-for i in range(1, 10):
+Exercise 8: Print the following pattern
+1
+22
+333
+4444
+55555
+666666
+......
+
+ for i in range(1, 10):
     convert_i_to_str = str(i)
     print( i * convert_i_to_str)
 
