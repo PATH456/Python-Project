2f4f10097783699a30b6f5aaa9fa7570779c3394
PATH456 <thanhhaipham652@gmail.com>
2024-07-03 12:40:21 +0700

    Update exercise (need help).py

--- exercise9.py
+++ exercise9.py
@@ -1,3 +1,7 @@
+Exercise 9: Check Palindrome Number
+Write a program to check if the given number is a palindrome number.
+
+A palindrome number is a number that is the same after reverse. For example, 545, is the palindrome numbers
 numbers = input('Enter a number here to check: ')
 if numbers[0] == numbers[-1] and numbers[1] == numbers[-2] and numbers[2] == numbers[-3] and numbers[3] == numbers[-4]:
     print('Yes, it is a palindrome number')
