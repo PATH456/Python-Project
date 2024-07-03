938ab26bcbed36d9d32ff0fbba86365677765d54
PATH456 <thanhhaipham652@gmail.com>
2024-07-03 10:22:10 +0700

    Update exercise6.py

--- exercise6.py
+++ exercise6.py
@@ -1,3 +1,6 @@
+Exercise 6: Display numbers divisible by 5 from a list
+Iterate the given list of numbers and print only those numbers which are divisible by 5
+
 Given_list =[10, 20, 33, 46 ,55]
 for i in Given_list:
     if i % 5 ==0:
