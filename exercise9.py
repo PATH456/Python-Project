e8cd1f8973d162829b93d265a2ab35cb705b3893
PATH456 <thanhhaipham652@gmail.com>
2024-07-03 12:39:48 +0700

    Add files via upload

---
+++ exercise9.py
@@ -0,0 +1,36 @@
+numbers = input('Enter a number here to check: ')
+if numbers[0] == numbers[-1] and numbers[1] == numbers[-2] and numbers[2] == numbers[-3] and numbers[3] == numbers[-4]:
+    print('Yes, it is a palindrome number')
+else:
+    print('No, it is not a palindrome number')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
