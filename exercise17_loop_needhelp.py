cff887dd5c77a8e2d05a5a112aed7967fd2a63a7
PATH456 <thanhhaipham652@gmail.com>
2024-07-14 11:36:03 +0700

    Update exercise17_loop_needhelp.py

--- exercise17_loop_needhelp.py
+++ exercise17_loop_needhelp.py
@@ -1,8 +1,15 @@
-num1 = 0
-num2 = 0
-result = int(input('Enter a number here: '))
-for i in range(result):
-    num1 = num1 * 10 + 2
+sum = 0
+next_num = 0
+
+n = int(input('Enter a number here: '))
+for i in range(n):
+    next_num = next_num*10+2
+    sum = sum + next_num
+    print("next_num: ", next_num)
+
+print("sum: ", sum)
+
+
 
 
 
