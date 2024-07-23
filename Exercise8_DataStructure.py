69408e44988c966498113f355425e1cd9297f1ce
PATH456 <thanhhaipham652@gmail.com>
2024-07-23 15:16:17 +0700

    Add files via upload

---
+++ Exercise8_DataStructure.py
@@ -0,0 +1,28 @@
+#Exercise 8: Iterate a given list and check if a given element exists as a key’s value in a dictionary.
+#If not, delete it from the list
+
+roll_number = [47, 64, 69, 37, 76, 83, 95, 97]
+sample_dict = {"John": 47, "Emma": 69, "Kelly": 76, "Jason": 97}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
