45a580cfcc233186d58b5dedb4747abdbb8840ac
PATH456 <thanhhaipham652@gmail.com>
2024-07-25 20:03:16 +0700

    Add files via upload

---
+++ Exercise4_DataStructure_needhelp.py
@@ -0,0 +1,48 @@
+#Exercise 4: Count the occurrence of each element from a list
+#Write a program to iterate a given list and count the occurrence of each element and create a dictionary to show the count of each element.
+
+sample_list = [11, 45, 8, 11, 23, 45, 23, 45, 89]
+count = 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
