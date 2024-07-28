bd4285891a982c4e8df1d9fe1025c14f60154c68
PATH456 <thanhhaipham652@gmail.com>
2024-07-28 11:23:33 +0700

    Update Exercise6_function_needhelp_done.py

--- Exercise6_function_needhelp.py
+++ Exercise6_function_needhelp.py
@@ -2,7 +2,18 @@
 #Write a program to create a recursive function to calculate the sum of numbers from 0 to 10.
 #A recursive function is a function that calls itself again and again.
 
+n = int(input())
+sum = 0
+def total(n, sum):
+    sum = sum + n
+    cal = n -1
+    if n-1 == 0:
+        return sum
+    else:
+        return total(cal, sum)
 
+result = total(n, sum)
+print(result)
 
 
 
