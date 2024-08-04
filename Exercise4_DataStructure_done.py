536e3dda0fd2168897d255f53685f121eb5d62f1
PATH456 <thanhhaipham652@gmail.com>
2024-08-04 11:05:29 +0700

    Update and rename Exercise4_DataStructure_needhelp.py to Exercise4_DataStructure_done.py

--- Exercise4_DataStructure_done.py
+++ Exercise4_DataStructure_done.py
@@ -2,9 +2,14 @@
 #Write a program to iterate a given list and count the occurrence of each element and create a dictionary to show the count of each element.
 
 sample_list = [11, 45, 8, 11, 23, 45, 23, 45, 89]
-for i in set(sample_list):
-    count = sample_list.count(i)
-    print("number from the list:", i, "Count:", count)
+result = dict()
+count = 1
+for i in sample_list:
+    if i not in result:
+        result[i] = count
+    else:
+        result[i] += 1
+print(result)
 
 
 
