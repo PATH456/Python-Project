218bad97b7239c3c19e53aab3498a507150ff7e5
PATH456 <thanhhaipham652@gmail.com>
2024-09-06 10:21:23 +0700

    Add files via upload

---
+++ Class_Variable_Ex2.py
@@ -0,0 +1,91 @@
+#Exercise 2: Student Enrollment
+
+#Requirements:
+
+    #Create a Student class with instance variables name and grade.
+    #Add a class variable school_name representing the name of the school.
+    #Add a class variable student_count to keep track of how many students have been enrolled.
+    #Create a method that prints the total number of students enrolled and the school name.
+class Student:
+    school_name = "International Melbourne"
+    student_count = 0
+    def __init__(self, name, grade, sex):
+        self.name = name
+        self.grade = grade
+        self.sex = sex
+        Student.student_count += 1
+    def stu_info(self):
+        print(f"Student information: {self.name}, grade {self.grade}, {self.sex}")
+
+stu1 = Student("Peter Parker", 12, "Male")
+stu2 = Student("Anna Taylor-Joy", 10, "Female")
+stu3 = Student("David Beckham", 11, "Male")
+
+print("School's Name:", Student.school_name)
+print("Total number of student:", Student.student_count)
+stu1.stu_info()
+stu2.stu_info()
+stu3.stu_info()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
