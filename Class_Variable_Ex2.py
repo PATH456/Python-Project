f1b5e16ea18ba2d9390d83541a8629c489515b6a
PATH456 <thanhhaipham652@gmail.com>
2024-09-09 09:52:57 +0700

    Update Class_Variable_Ex2.py

--- Class_Variable_Ex2.py
+++ Class_Variable_Ex2.py
@@ -15,7 +15,7 @@ class Student:
         self.sex = sex
         Student.student_count += 1
     def stu_info(self):
-        print(f"Student information: {self.name}, grade {self.grade}, {self.sex}")
+        print(f"Student information: {self.name}, grade {self.grade}, {self.sex}, {self.school_name}")
 
 stu1 = Student("Peter Parker", 12, "Male")
 stu2 = Student("Anna Taylor-Joy", 10, "Female")
@@ -29,15 +29,16 @@ stu2.stu_info()
 stu3.stu_info()
 
 
-#update class variable 
-#update object 
+#update class variable
+#update object
 
 
-school_name = "International Melbourne"
-stu1 = Student("Peter Parker", 12, "Male")
-Student.school_name = "High school"
-stu1.name= "Alan 
-#  class variable và attribute có trùng tên dc đươc không (ví dụ name)
+
+Student.name = "High school"
+stu1.name= "Alan"
+#  class variable và attribute có trùng tên dc đươc không (ví dụ name) => Possible. However, when we use self.name with an aim of printing the class variable "High school", the result would be the name of the object, not the name of the class variable. So, the only way is using Student.name instead
+# So, the only way is using Student.name instead
+stu1.stu_info()
 
 
 
